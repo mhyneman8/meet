23bc428f6a58e58ab352b855d9a6b699215234d7
mhyneman8 <mallory.hyneman@gmail.com>
2021-07-26 08:34:56 -0500

    4.4

--- src/App.js
+++ src/App.js
@@ -3,13 +3,31 @@ import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
+import { mockData } from './mock-data';
+import {  extractLocations, getEvents} from './api';
 
 class App extends Component {
+  state = {
+    events: [],
+    locations: []
+  }
+
+  updateEvents = (location) => {
+    getEvents().then((events) => {
+      const locationEvents = events.filter((event) => event.location === location);
+      this.setState({
+        events: locationEvents
+      });
+    });
+  }
+
   render() {
+    
     return (
+      
       <div className="App">
-        <CitySearch />
-        <EventList />
+        <CitySearch  locations={this.state.locations} updateEvents={this.updateEvents}/>
+        <EventList events={this.state.events} />
         <NumberOfEvents />
       </div>
     );  
