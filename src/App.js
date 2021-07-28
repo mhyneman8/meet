9e6180d68c02d3c417a93482766a5f7b26c3f7f2
mhyneman8 <mallory.hyneman@gmail.com>
2021-07-28 18:38:58 -0500

    4.4 fixes

--- src/App.js
+++ src/App.js
@@ -1,31 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
-import './nprogress.css';
-import axios from 'axios';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
-// import { mockData } from './mock-data';
 import { extractLocations, getEvents } from './api';
-// import NProgress from 'nprogress';
-
 
 class App extends Component {
   state = {
     events: [],
-    locations: []
+    locations: [],
+    numberOfEvents: 15
   }
 
-  // checkToken() {
-  //   const checkToken = async (accessToken) => {
-  //     const result = await fetch(
-  //       `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
-  //     )
-  //       .then((res) => res.json())
-  //       .catch((error) => error.json());
-  //     return result;
-  //   };
-  // }
 // checked 
   updateEvents = (location) => {
     getEvents().then((events) => {
@@ -40,27 +26,6 @@ class App extends Component {
     });
   }
 
-  // getAccessToken() {
-  //   const accessToken = localStorage.getItem('access_token');
-  //   const tokenCheck = accessToken && (await checkToken(accessToken));
-
-  //   if (!accessToken || tokenCheck.error) {
-  //     await localStorage.removeItem("access_token");
-
-  //     const searchParams = new URLSearchParams(window.location.search);
-  //     const code = await searchParams.get("code");
-
-  //     if (!code) {
-  //       const results = await axios.get("https://k3o9f64iji.execute-api.us-west-2.amazonaws.com/dev/api/get-auth-url");
-
-  //       const { authUrl } = results.data;
-  //       return (window.location.href = authUrl);
-  //     };
-  //     return code && getAccessToken(code);
-  //   } 
-  //   return accessToken;
-  // }
-
 // checked
   componentDidMount() {
     this.mounted = true;
@@ -73,10 +38,8 @@ class App extends Component {
   componenetWillUnmount(){
     this.mounted = false;
   }
-  
 
   render() {
-    
     return (
       
       <div className="App">
