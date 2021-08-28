cdf264fcb08c75032939abb7780f36107a47b873
mhyneman8 <mallory.hyneman@gmail.com>
2021-08-28 11:10:18 -0500

    Revert "Revert "add loader""
    This reverts commit 138468ed64bfc70386e1991d61678d7bad6111e9.

--- src/App.js
+++ src/App.js
@@ -4,16 +4,17 @@ import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 import { extractLocations, getEvents } from './api';
+import Loader from './Loader';
 
 class App extends Component {
   state = {
     events: [],
     locations: [],
     numberOfEvents: 15,
-    currentCity: 'all'
+    currentCity: 'all',
+    loading: false
   }
 
-// checked 
   updateEvents = (location, numberOfEvents) => {
     console.log('events');
     getEvents().then((events) => {
@@ -45,9 +46,12 @@ class App extends Component {
     this.updateEvents(currentCity, eventNumber);
   }
 
+  componentWillMount() {
+    this.setState({ loading: true });
+  }
 
-// checked
   componentDidMount() {
+    this.setState({ loading: false });
     const { numberOfEvents } = this.state;
     this.mounted = true;
     getEvents().then((events) => {
@@ -59,12 +63,13 @@ class App extends Component {
     });
   }
 
-  // checked
   componenetWillUnmount(){
     this.mounted = false;
   }
 
   render() {
+    if (this.state.loading)
+      return <Loader />
     return (
       
       <div className="App">
