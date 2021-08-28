055810ae720d8b0942d013d15d77c250cc18e344
mhyneman8 <mallory.hyneman@gmail.com>
2021-08-27 19:00:21 -0500

    Revert "4.8 added welcome screen"
    This reverts commit 038c0280e29bf07a2cf4d798d45ad09773b93793.

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -9,18 +9,19 @@ function WelcomeScreen(props) {
             <h4>
                 Log in to see upcoming events around the world for full-stack developers
             </h4>
-            <div className="button_cont" align="center">
-                <div className="google-btn">
-                    <div className="google-icon-wrapper">
+            <div classNamee="button_cont" align="center">
+                <div class="google-btn">
+                    <div class="google-icon-wrapper">
                         <img 
-                            className="google-icon"
-                            src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+                            class="google-icon"
+                            src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Log
+                            o.svg"
                             alt="Google sign-in"
                         />
                     </div>
                     <button onClick={() => { props.getAccessToken() }}
                         rel="nofollow noopener"
-                        className="btn-text"
+                        class="btn-text"
                     >
                         <b>Sign in with google</b>
                     </button>
