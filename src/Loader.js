5b73a10b8d90e2f859451bd891237ba0d8f86bff
mhyneman8 <mallory.hyneman@gmail.com>
2021-08-27 19:10:57 -0500

    Revert "spinner fix"
    This reverts commit a862353e17bebc535e05eaccd39e132cdccdabcc.

--- src/Loader.js
+++ src/Loader.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import Spinner from './img/Spinner.svg';
 
 class Loader extends Component {
     
     render() {
         return (
             <div className="loader">
-                <img src={Spinner} alt="loading spinner"></img>
+                <img src="./img/Spinner.svg" alt="loading spinner"></img>
             </div>
         )
     }
