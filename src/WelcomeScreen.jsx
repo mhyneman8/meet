bdb7f135f0fdde664dcd06979aed09f13387b411
mhyneman8 <mallory.hyneman@gmail.com>
2021-10-18 08:37:10 -0500

    update welcome screen

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -5,34 +5,40 @@ function WelcomeScreen(props) {
     return props.showWelcomeScreen ?
     (
         <div className="WelcomeScreen">
+            <div className="welcome-image"></div>
             <h1 className="welcome">Welcome to the Meet app</h1>
-            <h4 className="wel-text">
-                Log in to see upcoming events around the world for full-stack developers
-            </h4>
-            <div className="button_cont" align="center">
-                <div className="google-btn">
-                    <div className="google-icon-wrapper">
-                        <img 
-                            className="google-icon"
-                            src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
-                            alt="Google sign-in"
-                        />
-                    </div>
-                    <button onClick={() => { props.getAccessToken() }}
-                        rel="nofollow noopener"
-                        className="btn-text"
-                    >
-                        <b>Sign in with google</b>
-                    </button>
-                </div>
-            </div>
-            <a
-                href="https://mhyneman8.github.io/meet/privacy.html"
-                rel="nofollow noonpener"
-            >
-                Privacy Policy
-            </a>
-        </div>
+            <img src="./img/computerCode.jpg" alt="" />
+            
+            <div className="welcome-para">
+                <h4 className="wel-text">
+                    Log in to see upcoming events around the world for full-stack developers
+                </h4>
+                <div className="button_cont" align="center">
+                    <div className="google-btn">
+                        <div className="google-icon-wrapper">
+                            <img 
+                                className="google-icon"
+                                src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+                                alt="Google sign-in"
+                            />
+                        </div>
+                        <button onClick={() => { props.getAccessToken() }}
+                            rel="nofollow noopener"
+                            className="btn-text"
+                        >
+                            <b>Sign in with google</b>
+                        </button>
+                    </div> {/* end of google-btn */}
+                </div> {/* end of button_cont */}
+            
+                <a
+                    href="https://mhyneman8.github.io/meet/privacy.html"
+                    rel="nofollow noonpener"
+                >
+                    Privacy Policy
+                </a>
+            </div> {/* end of welcome-para */}
+        </div> 
     )
     : null
 }
